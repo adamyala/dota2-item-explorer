33471775c5e70a6e094c8f9d763707051848788c
Adam Yala <adamkyala@gmail.com>
2021-02-09 18:50:46 -0700

    working attribute and cost filters

--- index.js
+++ index.js
@@ -4,25 +4,58 @@ let appData = {
     attributes: attributes,
     items: items,
 
-    filteredAttributes: [],
-    filteredItems: items
+    filterAttributes: [],
+    filterCostSort: null,
+
+    filteredItems: items,
+  },
+  methods: {
+    filterItemsByAttribute: function (selectedAttributes) {
+      this.filteredItems = this.sortItems(this.items);
+
+      this.filteredItems = this.filteredItems.filter(
+
+        function (item) {
+          if (!selectedAttributes) {
+            return item
+          }
+
+          if (!item.attributesArray) {
+            return
+          }
+
+          let filterIsSubset = selectedAttributes.every(val => item.attributesArray.includes(val));
+          if (filterIsSubset) {
+            return item
+          }
+        }
+
+      );
+
+    },
+    sortItemsByCost: function (costSort) {
+        this.filteredItems = this.sortItems(this.filteredItems);
+    },
+    sortItems: function (items) {
+      if (this.filterCostSort === "false") {
+        return items.sort(
+          function (a, b) {
+            return a.cost - b.cost
+          }
+        )
+      } else {
+        return items.sort(
+          function (a, b) {
+            return b.cost - a.cost
+          }
+        )
+      }
+    }
   }
 };
 
-var app = new Vue(appData)
+let app = new Vue(appData)
 
-app.$watch('filteredAttributes', function (selectedAttributes) {
-  this.filteredItems = this.items.filter(
-    function (item) {
-      // debugger;
-      if (!item.attributesArray) {
-        return
-      }
+app.$watch('filterAttributes', app.filterItemsByAttribute)
 
-      let filterIsSubset = selectedAttributes.every(val => item.attributesArray.includes(val));
-      if (filterIsSubset) {
-        return item
-      }
-    }
-  );
-})
+app.$watch('filterCostSort', app.sortItemsByCost)
